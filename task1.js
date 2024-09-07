0f212c05b11873301c135d09ac2b73e5f14b1f70
ashraful2871 <ashrafulislam2758@gmail.com>
2024-09-07 13:29:20 +0600

    first commit

---
+++ task1.js
@@ -0,0 +1,9 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function fourNumberMulti(a, b, c, d) {
+  const Multiply = a * b * c * d;
+  return Multiply;
+}
+
+const numbers = fourNumberMulti(2, 4, 8, 5);
+console.log(numbers);
